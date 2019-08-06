0602b7c83025b04712a8a8d5dc0b3b44a8f3dada
phamhung21 <pham.vanhung.963871@gmail.com>
2019-08-06 11:39:27 +0700

    commmit form the_coder

--- routes/Customers.route.js
+++ routes/Customers.route.js
@@ -8,9 +8,13 @@ customer.delete('/deleteCustomer/:id', controller.delete);
 
 customer.get('/searchCustomer/:id', controller.search);
 
+customer.get('/searchCustomerByName', controller.searchByName)
+
 customer.post('/createCustomer', controller.create);
 
 customer.put('/updateCustomer/:id', controller.update);
+
+customer.get('/pagination/:page', controller.pagination);
 
 
 module.exports = customer
