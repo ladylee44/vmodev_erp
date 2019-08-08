d5a4f2214e23c62d950cb66ac00831ebe88068ba
ladylee_44 <thaohanu.knyt@gmail.com>
2019-08-08 08:50:34 +0700

    edit route employee

--- routes/Employees.route.js
+++ routes/Employees.route.js
@@ -2,12 +2,12 @@ const express = require('express');
 const employees = express.Router();
 const controller = require('./../controllers/Employees.controllers');
 
-employees.get('/employee/listEmployee', controller.employeeList);
-employees.get('/employee/listEmployee/:employeeid', controller.findEmployee);
-employees.get('/employee/search', controller.searchEmployeesByName);
+employees.get('/employee/list', controller.employeeList);
+employees.get('/employee/list/:employeeid', controller.findEmployee);
+employees.get('/employee/search/:name', controller.searchEmployeesByName);
 employees.get('/employee/pagination/:page', controller.pagination);
-employees.post('/employee/createEmployee', controller.addEmployee);
-employees.put('/employee/updateEmployee/:employeeid', controller.updateEmployee);
-employees.delete('/employee/deleteEmployee/:employeeid', controller.deleteEmployee);
+employees.post('/employee/create', controller.addEmployee);
+employees.put('/employee/update/:employeeid', controller.updateEmployee);
+employees.delete('/employee/delete/:employeeid', controller.deleteEmployee);
 
 module.exports = employees;
